daeae9bd21375cf23d2fc173c0243e50213307e6
Brian Szucs <brian@zooxmusic.com>
2024-02-19 21:08:49 -0500

    "fixe dpublish"

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo cp -f .bash_aliases /Users/zooxmusic/.bash_aliases
-sudo cp -f .bash_env /Users/zooxmusic/.bash_env
-sudo cp -f .bash_funcs /Users/zooxmusic/.bash_funcs
-sudo cp -f .bash_profile /Users/zooxmusic/.bash_profile
-sudo cp -f .bashrc /Users/zooxmusic/.bashrc;
-sudo . /Users/zooxmusic/.bashrc
+cp -f .bash_aliases /Users/zooxmusic/.bash_aliases
+cp -f .bash_env /Users/zooxmusic/.bash_env
+cp -f .bash_funcs /Users/zooxmusic/.bash_funcs
+cp -f .bash_profile /Users/zooxmusic/.bash_profile
+cp -f .bashrc /Users/zooxmusic/.bashrc;
+. /Users/zooxmusic/.bashrc
